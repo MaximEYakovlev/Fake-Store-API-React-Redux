58e602f13cbbe3db886106998d6118e6dd24f039
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-17 00:26:19 +0300

    created the entire redux infrastructure

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,14 @@
+import { actionTypes } from "../constants/action-types";
+
+const initialState = {
+  products: [],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
